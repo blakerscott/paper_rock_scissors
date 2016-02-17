d48a72d51d28f94d2a383ded0a30d5f7b99820d1
Blake Scott and Jon Nardine <blakerscott@gmail.com>
2016-02-17 09:43:37 -0800

    trying to save

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -4,39 +4,15 @@
 
 			function playGame($first_input, $second_input){
 
-			if ($first_input == "rock" && $second_input == "scissors")
+			if ($first_input == "rock" && $second_input == "scissors" || $first_input == "paper" && $second_input == "rock" || $first_input == "scissors" && $second_input == "paper")
 			{
 					return "Player 1";
 			}
-			elseif ($first_input == "rock" && $second_input == "paper")
+			elseif ($first_input == "rock" && $second_input == "paper" || $first_input == "paper" && $second_input == "scissors" || $first_input == "scissors" && $second_input == "rock")
 			{
 					return "Player 2";
 			}
-			elseif ($first_input == "paper" && $second_input == "rock")
-			{
-					return "Player 1";
-			}
-			elseif ($first_input == "paper" && $second_input == "scissors")
-			{
-					return "Player 2";
-			}
-			elseif ($first_input == "scissors" && $second_input == "rock")
-			{
-					return "Player 2";
-			}
-			elseif ($first_input == "scissors" && $second_input == "paper")
-			{
-					return "Player 1";
-			}
-			elseif ($first_input == "rock" && $second_input == "rock")
-			{
-					return "Draw";
-			}
-			elseif ($first_input == "scissors" && $second_input == "scissors")
-			{
-					return "Draw";
-			}
-			elseif ($first_input == "paper" && $second_input == "paper")
+			else 
 			{
 					return "Draw";
 			}
